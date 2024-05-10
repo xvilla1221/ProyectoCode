6aad30a5a94298062f9bb8280029b533506e0ef9
xvilla1221 <162973440+xvilla1221@users.noreply.github.com>
2024-05-10 09:32:17 +0200

    Add files via upload

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Biblioteca"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Pelicula"},{"p":"<Unnamed>","l":"Resenna"}];updateSearchResults();
